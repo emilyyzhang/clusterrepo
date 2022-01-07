e4e8c1eaf6a9485c519556ca8ba9c05d143cbdb3
Emily Zhang <eyzhang@mit.edu>
2022-01-07 15:54:58 -0500

    modify script

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -5,8 +5,9 @@
 #SBATCH --time=0-00:10
 #SBATCH -o /home/username/clusterrepo/myoutputfile.out
 #SBATCH -e /home/username/clusterrepo/myerrorfile.err
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=username@mit.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=eyzhang@mit.edu
+#SBATCH --output=trial.log
 
 module load julia/1.5.2
 
